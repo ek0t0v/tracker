3fdd2c6d62e08c7558aa522996a22db90f848667
Евгений Котов <dev@seniorcote.com>
2018-11-16 00:42:47 +0300

    Fix request retry

--- frontend/src/modules/api.js
+++ frontend/src/modules/api.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
 import store from '../store';
-
-// todo: Пофиксить, вроде бы обнаружил вылет при ошибке валидации (422).
-// todo: При 401 ошибке надо повторить все запросы, не только первый (кто первый успел, у того запрос повторился).
-
-let retry = false;
+import httpRetryQueue from './http-retry-queue';
 
 export const api = axios.create({
     baseURL: 'http://localhost/api',
@@ -15,42 +11,42 @@ export const api = axios.create({
     timeout: 5000,
 });
 
-api.interceptors.request.use(config => {
-    config.headers.Authorization = 'Bearer ' + localStorage.getItem('accessToken');
+api.interceptors.request.use(response => {
+    response.headers.Authorization = 'Bearer ' + localStorage.getItem('accessToken');
 
-    return config;
+    return response;
 }, error => Promise.reject(error));
 
-api.interceptors.response.use(null, error => {
+api.interceptors.response.use(response => response, error => {
     if (error.response.status === 401 && error.response.data.message === 'Bad credentials') {
         return Promise.reject(error);
     }
 
-    if (error.config && error.response.status === 401 && !retry) {
-        retry = true;
-
-        return api.post('/token/refresh', {
+    if (error.config && error.response.status === 401 && !httpRetryQueue.hasRequestsInQueue()) {
+        api.post('/token/refresh', {
             refresh_token: localStorage.getItem('refreshToken'),
         })
             .then(response => {
                 localStorage.setItem('accessToken', response.data.token);
                 localStorage.setItem('refreshToken', response.data.refresh_token);
-
-                error.config.headers.Authorization = 'Bearer ' + response.data.token;
-
-                return api.request(error.config);
+                httpRetryQueue.retryAll();
             })
             .catch(error => {
-                Promise.reject(error);
-
                 store.dispatch('user/userLogout');
+
+                return Promise.reject(error);
             })
         ;
     }
 
-    retry = false;
+    return httpRetryQueue.pushRetryFn({
+        reason: '401',
+        retryFn: () => {
+            error.config.headers.Authorization = 'Bearer ' + localStorage.getItem('accessToken');
 
-    return Promise.reject(error);
+            return axios(error.config);
+        }
+    });
 });
 
 export default api;
