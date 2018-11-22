3baa2557f2fda02df9ebfdd712c537a0c3b46071
Евгений Котов <dev@seniorcote.com>
2018-11-22 03:58:15 +0300

    WIP

---
+++ new-frontend/webpack.dev.js
@@ -0,0 +1,12 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+    mode: 'development',
+    devServer: {
+        contentBase: './public',
+        compress: true,
+        host: '0.0.0.0',
+        port: 8080,
+    },
+});
